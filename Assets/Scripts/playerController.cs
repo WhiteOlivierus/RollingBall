fcd1388eea7b2b85053965f0e47dae76d8122042
bastien <bas10olivier@hotmail.com>
2018-02-16 10:37:47 +0100

    Initial commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float rotateSpeed = 1;
+    private Rigidbody rb;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * moveSpeed);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * moveSpeed);
+        }else if (Input.GetKey(KeyCode.A)){
+            rb.AddTorque(-transform.up * rotateSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            rb.AddTorque(transform.up * rotateSpeed);
+        }
+    }
+}
